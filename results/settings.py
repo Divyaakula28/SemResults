8b59b27738e80e81bb22352ab3b29677a2b30db9
Divyaakula28 <divya2000akula@gmail.com>
2023-07-12 11:55:27 +0530

    settingsstaticupdate

--- results/settings.py
+++ results/settings.py
@@ -132,13 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os 
-
-# At the end of file. add these lines
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
