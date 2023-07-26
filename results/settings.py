479ecb69a9f93e0fc95a9360a1ea8ed75d8149e2
Divyaakula28 <divya2000akula@gmail.com>
2023-07-26 13:37:36 +0530

    staticupdate

--- results/settings.py
+++ results/settings.py
@@ -130,13 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os 
 
 STATIC_URL = '/static/'
-
 # settings.py
-import os 
 STATICFILE_DIRS=[
-    os.path.join(BASE_DIR,'twotwo/static')
+    os.path.join(BASE_DIR,'results/twotwo/ static')
 ]
-
-
